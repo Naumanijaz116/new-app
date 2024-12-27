f7e426affb2efdbfb16b95723c03464dc30aeed2
Naumanijaz116 <naumanijaz116@yahoo.com>
2024-12-27 23:32:14 +0500

    [my first props]

---
+++ src/components/arraydata.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+let arr[ 44 , 35 , 55 , 77 , 32 , 40]
+
+const arraydata = () => {
+  return (
+    <>
+      <h1>My Array Data</h1>
+console.log(arr)
+    </>
+  )
+}
+
+export default arraydata
