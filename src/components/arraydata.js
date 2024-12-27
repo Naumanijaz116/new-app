0156fa41e705f75606d3fd28285c154e44d79677
Naumanijaz116 <naumanijaz116@yahoo.com>
2024-12-27 23:54:24 +0500

    [2nd props n useStste]

--- src/components/arraydata.js
+++ src/components/arraydata.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-let arr[ 44 , 35 , 55 , 77 , 32 , 40]
+let a = arr ['44','35','55','77','32','40'];
 
 const arraydata = () => {
   return (
     <>
       <h1>My Array Data</h1>
-console.log(arr)
+console.log(a)
     </>
   )
 }
