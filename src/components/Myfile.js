0d5b72629aa60ab8afbb3ae62f7e7c2060f134d7
Naumanijaz116 <naumanijaz116@yahoo.com>
2024-12-28 00:24:40 +0500

    [props exe file]

---
+++ src/components/Myfile.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+let arr = ['44','35','55','77','32','40'];
+
+const Myfile = () => {
+  return (
+    <>
+      <h1>My Array Data</h1>
+      
+      console.log(arr)
+    </>
+  )
+}
+
+export default Myfile
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import arraydata from './components/arraydata'
+import Myfile from '../components/Myfile';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <arraydata />
+    <Myfile />
   </React.StrictMode>
 );
 
